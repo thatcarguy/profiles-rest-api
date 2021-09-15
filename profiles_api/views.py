2f84d8c79c599ab08c1f7de3c2f12d8c105127f0
thatcarguy <5158433+thatcarguy@users.noreply.github.com>
2021-09-15 12:19:15 -0600

    Added searching to user profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
@@ -95,9 +96,12 @@ class HelloViewSet(viewsets.ViewSet):
 # queryset name
 # made it use tokenauthentication
 # Has it check permissions from our UpdateOwnProfile permissions class
+# added filter backend. Can have more than one
 class UserProfilesViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
