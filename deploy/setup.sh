9ee356242ee5b37c8a8fe079665458a52b0f569a
thatcarguy <5158433+thatcarguy@users.noreply.github.com>
2021-09-15 23:53:10 -0600

    fixed typo

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -11,7 +11,7 @@ echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
 
-echo"Finished installing dependencies..."
+echo "Finished installing dependencies..."
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
